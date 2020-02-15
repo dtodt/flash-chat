2224e9b05731e87947cb2254f8a0f5ebf7ef6eae
dtodt <danieltodt@gmail.com>
2020-02-14 22:45:34 -0300

    feat: loading button on login and registration

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import 'chat_screen.dart';
 import 'registration_screen.dart';
@@ -18,74 +19,86 @@ class _LoginScreenState extends State<LoginScreen> {
 
   String email;
   String password;
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              child: Container(
-                height: 200.0,
-                child: Image.asset('assets/images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: loading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+                tag: 'logo',
               ),
-              tag: 'logo',
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                setState(() {
-                  password = value;
-                });
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password.',
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              color: Colors.lightBlueAccent,
-              onPressed: () async {
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(email: email, password: password);
-                  if (user != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  } else {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password.',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                color: Colors.lightBlueAccent,
+                onPressed: () async {
+                  setState(() {
+                    loading = true;
+                  });
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    } else {
+                      Navigator.pushNamed(context, RegistrationScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
+                  } finally {
+                    setState(() {
+                      loading = false;
+                    });
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-              label: 'Log In',
-            ),
-          ],
+                },
+                label: 'Log In',
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import 'chat_screen.dart';
 
@@ -17,72 +18,84 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
 
   String email;
   String password;
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              child: Container(
-                height: 200.0,
-                child: Image.asset('assets/images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: loading,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+                tag: 'logo',
               ),
-              tag: 'logo',
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                setState(() {
-                  password = value;
-                });
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              color: Colors.blueAccent,
-              onPressed: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                color: Colors.blueAccent,
+                onPressed: () async {
+                  setState(() {
+                    loading = true;
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
+                  } finally {
+                    setState(() {
+                      loading = false;
+                    });
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-              label: 'Register',
-            ),
-          ],
+                },
+                label: 'Register',
+              ),
+            ],
+          ),
         ),
       ),
     );
